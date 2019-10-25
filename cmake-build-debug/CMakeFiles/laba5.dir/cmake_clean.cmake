e2bff1baa10bb99cc0f7e70fb12f495c8c0e2b89
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-25 21:56:30 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/laba5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5.dir/main.cpp.o"
+  "laba5.exe"
+  "laba5.pdb"
+  "liblaba5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
