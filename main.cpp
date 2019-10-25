05773b1dfd5578acfc188a8ce5ca02353bc436ea
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-25 22:15:28 +0300

    Code is full working, but it needs coments for my teacher.(

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
+void palindrome1();
+
+void palindrome2();
+
+void palindrome3();
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    palindrome1();
+    palindrome2();
+    palindrome3();
     return 0;
+}
+
+void palindrome1() {
+    for (int i = 1; i < 10; i++) {
+        cout << i << ", ";
+    }
+    cout << endl;
+}
+
+void palindrome2() {
+    for (int i = 1; i < 10; i++) {
+        cout << 11 * i << ", ";
+    }
+    cout << endl;
+}
+
+void palindrome3() {
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << i << j << i << ", ";
+        }
+        cout << endl;
+    }
 }
