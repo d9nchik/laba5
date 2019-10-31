3e36dad21d54467868b05e61d9745580577aa897
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-31 19:00:45 +0200

    My previous code was bull shit, so I`ve done better program with comments.

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main() {
 
-    int maximum = 1000;
+    int maximum = 1000;//Змінна що несе значееня максимального паліндрому
     cout << "Palindrome Numbers between 1 and " << maximum << " are :";
 
     for (int num = 1; num < maximum + 1; ++num) {
-        int temp = num;
-        int reverse = 0;
+        int temp = num;//Створюємо копію num з якою будемо працювати
+        int reverse = 0;//Тут будемо зберігати реверсоване число.
 
         while (temp > 0) {
-            int Reminder = temp % 10;
+            int Reminder = temp % 10;//змінна для збереження кожного символу
             reverse = (reverse * 10) + Reminder;
             temp = temp / 10;
         }
