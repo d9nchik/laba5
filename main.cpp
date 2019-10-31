5344147b920ef224a182cfb8bdfdcbd49e6b3126
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-31 18:56:25 +0200

    My previous code was bull shit, so I`ve done better program.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i < 10; i++) {
-        cout << i << ", ";
-    }
-    cout << endl;
 
-    for (int i = 1; i < 10; i++) {
-        cout << 11 * i << ", ";
-    }
-    cout << endl;
+    int maximum = 1000;
+    cout << "Palindrome Numbers between 1 and " << maximum << " are :";
 
-    for (int i = 1; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            cout << i << j << i << ", ";
+    for (int num = 1; num < maximum + 1; ++num) {
+        int temp = num;
+        int reverse = 0;
+
+        while (temp > 0) {
+            int Reminder = temp % 10;
+            reverse = (reverse * 10) + Reminder;
+            temp = temp / 10;
         }
-        cout << endl;
+        if (num == reverse) {
+            cout << num << " ";
+        }
+
+
     }
 
     return 0;
